fd268865729593b05ed99f822ec29b080147c30f
DillahAmaliaFiqyani <dillah.amalia.f@gmail.com>
2021-07-14 22:36:45 +0700

    ktp

--- app/src/main/java/com/pemkabjember/disdukcapil/MainActivity.java
+++ app/src/main/java/com/pemkabjember/disdukcapil/MainActivity.java
@@ -2,13 +2,24 @@ package com.pemkabjember.disdukcapil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< HEAD
+
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, AktaActivity.class)));
+=======
+        int a;
+>>>>>>> bfee79c6385bcbcfad62cb5d6b8f369150fbd1c8
     }
 }

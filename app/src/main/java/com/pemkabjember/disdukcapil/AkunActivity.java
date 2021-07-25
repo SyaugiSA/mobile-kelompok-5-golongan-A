ea18725c41047bf235b36f2055c8e5c986c5f3df
Syaugi Salim Amar <71821834+SSA5461@users.noreply.github.com>
2021-07-25 23:35:40 +0700

    update akun dan main activity

--- app/src/main/java/com/pemkabjember/disdukcapil/AkunActivity.java
+++ app/src/main/java/com/pemkabjember/disdukcapil/AkunActivity.java
@@ -2,7 +2,10 @@ package com.pemkabjember.disdukcapil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AkunActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class AkunActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_akun);
+
+        findViewById(R.id.button_keluar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Preferences.ClearLoggedInUser(getBaseContext());
+                startActivity(new Intent(getBaseContext(),LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/pemkabjember/disdukcapil/MainActivity.java
+++ app/src/main/java/com/pemkabjember/disdukcapil/MainActivity.java
@@ -3,24 +3,67 @@ package com.pemkabjember.disdukcapil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.time.Instant;
 
 public class MainActivity extends AppCompatActivity {
-    private Preferences preferences;
-    private Button button1;
+    TextView user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notLogedIn();
 
-        preferences = new Preferences(this);
+        user = findViewById(R.id.bg_header);
+        user.setText(Preferences.getLoggedInUser(getBaseContext()));
 
-        button1 = findViewById(R.id.button1);
+        findViewById(R.id.btn_buka).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://wsjti.id/Bersama/public/")));
+            }
+        });
 
-        if (preferences.getId() == null || preferences.getId().isEmpty()){
-            startActivity(new Intent(getBaseContext(), LoginActivity.class));
+        findViewById(R.id.btn_akta).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), AktaActivity.class));
+            }
+        });
+
+        findViewById(R.id.btn_ktp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), KartuTandaPendudukActivity.class));
+            }
+        });
+
+        findViewById(R.id.btn_home).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+            }
+        });
+
+        findViewById(R.id.btn_akun).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), AkunActivity.class));
+            }
+        });
+    }
+
+    private void notLogedIn(){
+        if (!Preferences.getLoggedInStatus(getBaseContext())){
+            startActivity(new Intent(getBaseContext(),LoginActivity.class));
+            finish();
         }
     }
 }
